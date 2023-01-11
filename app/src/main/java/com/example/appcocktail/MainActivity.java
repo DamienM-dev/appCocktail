658f790048b85197f545de84e4e0166eb675bcf9
Damien MIREMONT <damien.miremont@gmail.com>
2023-01-11 14:29:31 +0100

    first commit, create MainActivity and HomeActivity

---
+++ app/src/main/java/com/example/appcocktail/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.appcocktail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+//Au clique du bouton, je vais sur l'accueil
+        //TODO faire une condition pour connecter grace a l'email et pwd
+
+        Button button = findViewById(R.id.buttonConnexion);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
